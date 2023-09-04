8880ff26ec00b13475d98ef8813bc09659236bd7
Moballighul islam <moballigh5225@gmail.com>
2023-09-05 01:15:34 +0530

    created project and custom button

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      <span className={`flex-1`}>title</span>
+    </button>
+  );
+};
+
+export default CustomButton;
