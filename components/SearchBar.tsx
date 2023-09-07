f346f46aaf529936ec32cff7e9b2b0c70cd5c7f4
Moballighul islam <moballigh5225@gmail.com>
2023-09-08 01:57:09 +0530

    created and added the Search bar with filter,CarCards and Popup Modal with Car Details

---
+++ components/SearchBar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { SearchManufacturer } from "./";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+  const handleSearch = () => {};
+  return (
+    <form className="searchbar" onSubmit={handleSearch}>
+      <div className="searchbar__items">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,17 @@
 import Hero from "./Hero";
 import CustomButton from "./CustomButton";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
 
-export { Hero, CustomButton };
+export {
+  Hero,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+};
